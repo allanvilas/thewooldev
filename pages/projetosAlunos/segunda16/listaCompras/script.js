b315cc4aedd5617cca3beebbee4d2418358f89c4
allanvilas <allanvilasdeveloper@gmail.com>
2021-08-16 17:31:13 -0300

    adicionado projeto de js

---
+++ pages/projetosAlunos/segunda16/listaCompras/script.js
@@ -0,0 +1,22 @@
+//hospeda a lista onde serão adicionado as linhas
+var lista = document.getElementById('lista');
+//hospeda o botão de adicionar item
+var btnadd = document.getElementById('btnAdd');
+//adiciona um evento de click no botão
+btnadd.addEventListener('click',addItem);
+
+//função que adiciona linhas na lista
+function addItem(){
+    //cria uma nova linha no html
+    var novaLinha = document.createElement('li');
+    //adiciona texto a nova linha
+    novaLinha.innerHTML = "nova linhas";
+    novaLinha.addEventListener('click',function(){renomeiaItem(novaLinha)})
+    //adiciona a nova linha na lista
+    lista.appendChild(novaLinha);
+}
+
+function renomeiaItem(item){
+    var novoTexto = window.prompt("Insira o nome do Item");
+    item.innerHTML = novoTexto;  
+}
