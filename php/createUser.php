9d4003444377130104172d3e342b7c1248c8b2bb
allanvilas <allanvilasdeveloper@gmail.com>
2021-09-16 15:16:40 -0300

    adicionar script php

---
+++ php/createUser.php
@@ -0,0 +1,11 @@
+<?php
+$nome = $_POST['name'];
+$sobrenome = $_POST['sobrenome'];
+$nascimento = $_POST['nascimento'];
+$telefone = $_POST['telefone'];
+$user = $_POST['user'];
+$password = $_POST['pass1'];
+$email = $_POST['email'];
+
+echo $nome."<br>".$sobrenome."<br>".$nascimento."<br>".$telefone."<br>".$user."<br>".$password."<br>".$email."<br>";
+?>
