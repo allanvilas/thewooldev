22d81090023c0e5d00c1569e6c3254041f67d463
allanvilas <allanvilasdeveloper@gmail.com>
2021-09-16 15:58:40 -0300

    feito ajustes no script php

--- php/createUser.php
+++ php/createUser.php
@@ -1,11 +1,51 @@
-<?php
-$nome = $_POST['name'];
-$sobrenome = $_POST['sobrenome'];
-$nascimento = $_POST['nascimento'];
-$telefone = $_POST['telefone'];
-$user = $_POST['user'];
-$password = $_POST['pass1'];
-$email = $_POST['email'];
 
-echo $nome."<br>".$sobrenome."<br>".$nascimento."<br>".$telefone."<br>".$user."<br>".$password."<br>".$email."<br>";
+<?php
+    $nome = $_POST['name'];
+    $sobrenome = $_POST['sobrenome'];
+    $nascimento = $_POST['nascimento'];
+    $telefone = $_POST['telefone'];
+    $user = $_POST['user'];
+    $password = $_POST['pass1'];
+    $email = $_POST['email'];
+
+    function msg($msg){
+        echo '<script>';
+        echo 'console.log("'.$msg.'")';
+        echo '</script>';
+    }
+    $mysqli = new mysqli("172.106.0.120", "thewooldevadmin", "7436801aA@", "usersdata");
+        if ($mysqli->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+    if($mysqli->query("INSERT INTO `users`(`nome`, `sobrenome`, `nascimento`, `telefone`, `user`, `pass`, `email`) VALUES ('$nome','$sobrenome','$nascimento','$telefone','$user','$password','$email')") === true){
+        msg("inserted into database");
+    }
+    else{
+        msg("failed");
+    }
+
+    $mysqli->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <style>
+            html,body,h1{
+                color:#999 !important;
+            }
+        </style>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="../css/style.css">
+        <link type="text/css" rel="stylesheet" href="../pages/createAccount.css">
+        <link type="text/css" rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+        <title>Conta Criada</title>
+    </head>
+    <body>
+        <div class="container position-absolute top-50 start-50 translate-middle">
+            <h1>CONTA CRIADA COM SUCESSO<h1>
+            <a href="../index.html"><button class="btn btn-light mb-5">Voltar</button></a>
+        </div>
+    </body>
+</html>
