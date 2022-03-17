2eaba779b98834bff211b579f561e6548c578b31
allanvilas <allanvilasdeveloper@gmail.com>
2022-03-17 19:11:38 -0300

    teste da página jornada

--- pages/jornada/jornada.php
+++ pages/jornada/jornada.php
@@ -3,6 +3,12 @@
     include '../../php/header.php';
     include '../../php/menu.php';
 
-     echo 'teste php';
+     echo '
+     <h1>Projeto AGA.INNST</h1>
+     <h2>Descrição</h2>
+     <!-- Video -->
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/gpRT6pxz-fE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
      
+     ';
+    
 ?>
