4a761c0d057094f3ed75564cb09d029e72d6f56b
allanvilas <allanvilasdeveloper@gmail.com>
2022-03-17 19:45:00 -0300

    teste da página jornada

--- pages/jornada/jornada.php
+++ pages/jornada/jornada.php
@@ -43,6 +43,11 @@
                 </article>
             </section>
         </main>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script src="js/main.js"></script>
+        <script src="js/index.js"></script>
     </body>
      ';
 ?>
