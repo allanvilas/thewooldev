e366cd9f8424bc95d94145ddcbe09de0a5683ce7
allanvilas <allanvilasdeveloper@gmail.com>
2022-03-17 19:17:04 -0300

    teste da página jornada

--- pages/jornada/jornada.php
+++ pages/jornada/jornada.php
@@ -4,18 +4,20 @@
     include '../../php/menu.php';
 
      echo '
-     <main>
-        <section>
-            <article>
-            <div class="col-sm-0 col-md-1 col-lx-2">
-            <div class="col-sm-12 col-md-10 col-lx-8">
-               <h1>Projeto AGA.INNST</h1>
-               <h2>Descrição</h2>
-               <!-- Video -->
-               <iframe width="560" height="315" src="https://www.youtube.com/embed/gpRT6pxz-fE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            </article>
-        </section>
-     </main>
+    <body class="container-fluid">
+        <main class="mx-auto">
+            <section>
+                <article>
+                <div class="col-sm-0 col-md-1 col-lx-2">
+                <div class="col-sm-12 col-md-10 col-lx-8">
+                <h1>Projeto AGA.INNST</h1>
+                <h2>Descrição</h2>
+                <!-- Video -->
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/gpRT6pxz-fE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                </article>
+            </section>
+        </main>
+    </body>
      ';
 ?>
