34402c63e3a66f77d57931773faae2f78f4166e8
allanvilas <allanvilasdeveloper@gmail.com>
2022-03-17 19:23:54 -0300

    teste da página jornada

--- pages/jornada/jornada.php
+++ pages/jornada/jornada.php
@@ -11,9 +11,9 @@
                 <div class="col-sm-0 col-md-1 col-lx-2"></div>
                     <div class="col-sm-12 col-md-10 col-lx-8">
                         <h1>Projeto AGA.INNST</h1>
-                        <h2>De forma geral, a temática da mitologia nórdica tem se tornado cada vez mais popular entre o público jovem. São diversos os conteúdos midiáticos que fazem referência a esse assunto, como filmes de super-heróis, jogos de videogame, séries de televisão e livros. Apesar disso, no que tange a área de jogos físicos, como os de tabuleiro, essa adesão não é das maiores. 
+                        <p>De forma geral, a temática da mitologia nórdica tem se tornado cada vez mais popular entre o público jovem. São diversos os conteúdos midiáticos que fazem referência a esse assunto, como filmes de super-heróis, jogos de videogame, séries de televisão e livros. Apesar disso, no que tange a área de jogos físicos, como os de tabuleiro, essa adesão não é das maiores. 
                         Outro problema recorrente, é a falta de embasamento que muitos desses conteúdos apresentam, por vezes, os seres fantásticos que aparecem nessa literatura só possuem o nome como aspecto original de seu conteúdo. Associando os motivos apontados anteriormente, decidimos pôr em prática a ideia de produzir um projeto com tema proposto (viagem), relacionando com o sincretismo e a cultura dos povos que originaram essas histórias.
-                        </h2>
+                        </p>
                         <!-- Video -->
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/gpRT6pxz-fE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         <h2>
